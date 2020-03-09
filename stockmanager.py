15acaa2d8491f4975fed7062291d0da5d6088942
yongmin.ko <euclidmin@gmail.com>
2020-03-09 18:34:24 +0900

    functional programing style 변경

--- stockmanager.py
+++ stockmanager.py
@@ -20,10 +20,12 @@ sh_tran = wb.sheet_by_name('Transaction')
 sh_prod = wb.sheet_by_name('Product')
 
 # print(sh_prod)
-col0 = sh_prod.col(0)
+# col0 = sh_prod.col(0)
 ncol = sh_prod.ncols
 nrow = sh_prod.nrows
-print(col0)
+# print(col0)
+
+# colomn 개수, row 개수
 print(ncol)
 print(nrow)
 
@@ -32,7 +34,7 @@ def get_product(prod_id):
     for idx in range(1, sh_prod.nrows):
         rec_p = sh_prod.row(idx)
         print('rec_p[0]= {0} prod_id={1}'.format(rec_p[0], prod_id))
-        if (rec_p[0].value == prod_id.value) :
+        if (rec_p[0].value == prod_id) :
             ret = rec_p
             return ret
         else :
@@ -40,9 +42,18 @@ def get_product(prod_id):
     return ret
 
 def update_stock_cnt(rec_p, cnt, type):
-    if type.value == 'sell' : cnt.value *= -1
-    rec_p[6].value += cnt.value
+    if type == 'sell' : cnt *= -1
+    rec_p[6].value += cnt
 
+
+def get_product_ID(rec_t):
+    return rec_t[2].value
+
+def get_transaction_cnt(rec_t):
+    return rec_t[3].value
+
+def get_transaction_type(rec_t):
+    return rec_t[1].value
 
 
 #재고 계산
@@ -50,13 +61,16 @@ def update_stock_cnt(rec_p, cnt, type):
 for idx in range(1, sh_tran.nrows):
     rec_t = sh_tran.row(idx)
     print(rec_t)
-    rec_p = get_product(rec_t[2])                     # rec_t[2] =>  Product ID
-    update_stock_cnt(rec_p, rec_t[3], rec_t[1])       # rec_t[3] =>  Count , rec_t[1] => Type(sell/buy)
+    prod_id = get_product_ID(rec_t)
+    rec_p = get_product(prod_id)                     # rec_t[2] =>  Product ID
+    cnt = get_transaction_cnt(rec_t)
+    t_type = get_transaction_type(rec_t)
+    update_stock_cnt(rec_p, cnt, t_type)       # rec_t[3] =>  Count , rec_t[1] => Type(sell/buy)
 
-
-for idx in range(1, sh_prod.nrows):
-    rec_p = sh_prod.row(idx)
-    print(rec_p)
+#
+# for idx in range(1, sh_prod.nrows):
+#     rec_p = sh_prod.row(idx)
+#     print(rec_p)
 
 
 
