77066e2bdb40c2ed8a460f15f0bd3704dd75a413
yongmin.ko <euclidmin@gmail.com>
2020-04-02 16:00:20 +0900

    ui파일 연동 추가

--- stockmanager.py
+++ stockmanager.py
@@ -1,11 +1,18 @@
 import openpyxl
 from datetime import datetime
+from PySide2 import QtCore, QtWidgets, QtGui
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtCore import QFile
+import sys
+from PySide2.QtWidgets import QMainWindow, QWidget
+
 
 class StockManager:
     def __init__(self):
         self.workbook = None
         self.p_sheet = None
         self.t_sheet = None
+        self.m_sheet = None
         self.product_id = None
         self.type = None
         self.count = None
@@ -17,6 +24,8 @@ class StockManager:
         # self.p_sheet = self.workbook.get_sheet_by_name('Product')
         self.t_sheet = self.workbook['Transaction']
         self.p_sheet = self.workbook['Product']
+        self.m_sheet = self.workbook['Manufacturer']
+
 
     def _update_count(self, row, cnt):
         self.p_sheet['G' + str(row)] = self.p_sheet['G' + str(row)].value + cnt
@@ -52,12 +61,48 @@ class StockManager:
         fname = 'jiggingpro'+str(now.year)+'-'+str(now.month)+'-'+str(now.day)+'_'+str(now.hour)+str(now.minute)+str(now.second)+'.xlsx'
         self.workbook.save(fname)
 
+    def get_manufacturer(self):
+        manuf_cells = lambda colm: self.m_sheet[colm]
+        cell_value = lambda cell : cell.value
+
+        manufacturer_list = list(map(cell_value, manuf_cells('B')))
+        return manufacturer_list[1:]         # 첫줄의 카테고리 이름을 제외 시킨다.
 
 
-sm = StockManager()
-sm.open_excel()
-sm.update_stock()
-sm.save()
 
 
+
+
+
+
+
+
+
+class stock_window:
+    def __init__(self, stock_manager):
+        self.sm = stock_manager
+        self.ui_file = QFile("product_select.ui")
+        self.ui_file.open(QFile.ReadOnly)
+        self.loader = QUiLoader()
+        self.main_window = self.loader.load(self.ui_file)
+        self.ui_file.close()
+        self.main_window.show()
+
+        self.combobox_add_manufacturer()
+
+    def combobox_add_manufacturer(self):
+        cb = self.main_window.findChild(QtWidgets.QComboBox, "comboBox")
+        mf_list = self.sm.get_manufacturer()
+        cb.addItems(mf_list)
+
+
+if __name__ == '__main__':
+    sm = StockManager()
+    sm.open_excel()
+    # sm.update_stock()
+    # sm.save()
+    app = QtWidgets.QApplication(sys.argv)
+    sw = stock_window(sm)
+    sys.exit(app.exec_())
+    
 
